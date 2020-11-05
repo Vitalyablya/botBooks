5b2d8c6fad63b6982fb28b23806fb0b86a33dd0d
Vitalya <johndoe@example.com>
2020-11-05 17:06:30 +0300

    Значительно улучшена функция поиска книги

--- torCurl.php
+++ torCurl.php
@@ -1,12 +1,14 @@
-<?php 
-function getCurl($url, $proxy = PROXY) {
+<?php
+// Возвращает гет запрос через тор сеть
+// $url - url запроса $encoding -  кодирование $proxy - прокси
+function getCurl(string $url, string $encoding = "gzip", string $proxy = PROXY){
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_PROXY, $proxy);
     curl_setopt($ch, CURLOPT_PROXYTYPE, 7);
     curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
     curl_setopt($ch, CURLOPT_HEADER, 1);
-    curl_setopt($ch, CURLOPT_ENCODING , "gzip");
+    curl_setopt($ch, CURLOPT_ENCODING , $encoding);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, FALSE);
     curl_setopt($ch, CURLOPT_URL, $url);
     $curl_scraped_page = curl_exec($ch);
