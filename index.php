89035e5ad9569aecae38e814bb5ba08ca167fff0
Vitalya <johndoe@example.com>
2020-11-05 16:34:24 +0300

    Доработана система поиска

--- index.php
+++ index.php
@@ -6,10 +6,14 @@ require_once "parseBok.php";
 
 $list = 3;
 $countBox = 5;
-$listboks = getBooksUponReq("Гарри Поттер");
-$listboks_slice = array_slice($listboks, $list * $countBox, $countBox);
-$textMsg = "Выведено $countBox из " . count($listboks) . " (" . ($list * $countBox) . "-" . ($list * $countBox + $countBox) . ")\n";
-foreach($listboks_slice as $key => $val){ $textMsg .=$key + 1 .". ". trim($val[0]) . "\n\n"; 
+function searcheBook($nameBook, $list, $countBox){
+    $listboks = getBooksUponReq($nameBook);
+    if(!$listboks) return sendMsg("Ничего не найдено");
+    print_r($listboks);
+    $listboks_slice = array_slice($listboks, $list * $countBox, $countBox);
+    $textMsg = "Выведено $countBox из " . count($listboks) . " (" . ($list * $countBox) . "-" . ($list * $countBox + $countBox) . ")\n";
+    foreach($listboks_slice as $key => $val){ $textMsg .=$key + 1 .". ". trim($val[0]) . "\n\n"; 
+    }
+    sendMsg($textMsg);
 }
-sendMsg($textMsg);
 
--- parseBok.php
+++ parseBok.php
@@ -4,17 +4,28 @@ require_once "torCurl.php";
 function getBooksUponReq($namebook){
     $page = 0;
     $res = [];
+
     do{
         $url = HOSTSCRAPING . "/booksearch?page=$page&ask=" . urlencode($namebook) . "&chb=on";
         $html = getCurl($url);
+        if(!$html) break;
+        
         $doc = new DiDom\Document($html);
-        $doc = new DiDom\Document($doc -> find('#main.clear-block ul')[1] -> html());
+        $doc = $doc -> find('#main.clear-block ul');
+
+        if(isset($doc[1])) $doc = new DiDom\Document($doc[1] -> html());
+        else if(isset($doc[0])) $doc = new DiDom\Document($doc[0] -> html());
+        else break;
+
         $arr = $doc -> find("li"); 
         foreach($arr as $val){
             $res[] = [$val -> text(), $val -> children()[1] -> getAttribute('href')];
         }
+
         if(count($arr) < 50) break;
         $page++;
+
     }while(true);
-    return $res;
+
+    return !empty($res) ? $res : false;
 }
