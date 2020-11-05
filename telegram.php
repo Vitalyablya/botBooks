3cb2726bbd93ff7dac0cfacda97a4451f1c21b67
Vitalya <johndoe@example.com>
2020-11-05 18:54:53 +0300

    Подключение к хероку

--- telegram.php
+++ telegram.php
@@ -1,12 +1,6 @@
 <?php 
-$telegram = new Telegram\Bot\Api(TOKENBOT);
 // отправка сообщений в телеграмм
 function sendMsg($msg){
-    global $telegram;
-    $response = $telegram->sendMessage([
-        'chat_id' => USERID, 
-        'text' => $msg
-      ]);
-    $messageId = $response->getMessageId();
+
 }
 ?>
